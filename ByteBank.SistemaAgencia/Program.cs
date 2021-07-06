32b96f8efce52b29d3e028bf190453ef1b8bb4a7
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-06 20:50:36 -0300

    Projeto com biblioteca e referencia para biblioteca

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,21 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(847, 4989754);
+
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
