db270d47c97691d7a91abc7577f33c4ca8cd79cf
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-09 23:43:01 -0300

    Utilizando Humanaizer a partir do NuGet

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
+using Humanizer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +13,16 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(847, 4989754);
- 
-            FuncionarioAutenticavel carlos = null;
-            carlos.Autenticar("xssffgg");
+            DateTime dataFimPagamento = new DateTime(2021, 7, 17);
 
-            Console.WriteLine(conta.Numero);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(40);// dataFimPagamento - dataCorrente
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            Console.WriteLine(mensagem);
+
+            Console.WriteLine(dataCorrente);
+            Console.WriteLine(dataFimPagamento);
 
             Console.ReadLine();
         }
